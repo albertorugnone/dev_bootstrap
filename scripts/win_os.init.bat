c4f5e78d83b8ca95c4b8177dd7f6512f39d76d33
Alberto Rugnone <rugnonealberto@gmail.com>
2015-07-24 11:42:33 +0200

    added installing source tree

--- scripts/win_os.init.bat
+++ scripts/win_os.init.bat
@@ -1,1 +1,4 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+"installing chocolatey" >
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+"installing sourcetree" >
+choco install sourcetree -y
