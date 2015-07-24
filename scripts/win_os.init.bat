d8abbeb726f0a14b27028adf60b838a8a6d84ebe
Alberto Rugnone <rugnonealberto@gmail.com>
2015-07-24 11:55:53 +0200

    added intellij-ultimate installation

--- scripts/win_os.init.bat
+++ scripts/win_os.init.bat
@@ -2,3 +2,5 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 "installing sourcetree" >
 choco install sourcetree -y
+"installing intellijidea-ultimate" >
+choco install intellijidea-ultimate -y
