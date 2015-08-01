fcd8d8dfe88418376f04f7b77ced0322bb44b76a
Alberto Rugnone <rugnonealberto@gmail.com>
2015-08-01 12:22:40 +0200

    fix echos

--- scripts/win_os.init.bat
+++ scripts/win_os.init.bat
@@ -1,12 +1,12 @@
-"installing chocolatey" >
+echo "[WINDOWS INIT] installing chocolatey"
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
-"installing git" >
+echo "[WINDOWS INIT] installing git"
 choco install git -y
 choco install poshgit -y
-"installing git flow"
+echo "[WINDOWS INIT] installing git flow"
 choco install git-flow-dependencies -y
 choco install devbox-gitflow -y
-"installing sourcetree" >
+echo "[WINDOWS INIT] installing sourcetree"
 choco install sourcetree -y
-"installing intellijidea-ultimate" >
+echo "[WINDOWS INIT] installing intellijidea-ultimate"
 choco install intellijidea-ultimate -y
