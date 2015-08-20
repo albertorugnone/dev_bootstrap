2937515b90b188b9a6994ea7d153320e74023d38
Alberto Rugnone <rugnonealberto@gmail.com>
2015-08-20 16:11:52 +0200

    added list script for windows (WIP)

--- scripts/eclipse.list.bat
+++ scripts/eclipse.list.bat
@@ -2,7 +2,7 @@
 -clean -purgeHistory ^
 -application org.eclipse.equinox.p2.director ^
 -noSplash ^
--repository http://downloads.sonarsource.com/eclipse/eclipse/ ^
--list^
+-repository http://moreunit.sourceforge.net/update-site/ ^
+-list ^
 -vmargs -Declipse.p2.mirrors=true -Djava.net.preferIPv4Stack=true -Dequinox.scr.waitTimeOnBlock=10000 -Dequinox.ds.perf=true
 
