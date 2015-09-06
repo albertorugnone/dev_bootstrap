6f830224e0f8f7fcd486d0667afadd652ba95f9b
Alberto Rugnone <rugnonealberto@gmail.com>
2015-09-06 17:06:02 +0200

    added installation of conemu and its patch

--- scripts/patches.bat
+++ scripts/patches.bat
@@ -2,3 +2,6 @@ SET patchesName="[WINDOWS PATCHES]"
 
 echo %patchesName%  patching git installation
 cp "C:\Program Files (x86)\Git\bin\libiconv-2.dll" "C:\Program Files (x86)\Git\libexec\git-core\libiconv2.dll" 
+
+echo %patchesName%  patching conemu installation
+mv "C:\Program Files\ConEmu\ConEmu\ConEmu_Sample.xml"  "C:\Program Files\ConEmu\ConEmu\ConEmu.xml"
